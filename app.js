6ac13f2e4377528e3f69d0ca9c8501d182b948e1
brigihorvath <brigi.horvath82@gmail.com>
2021-05-28 14:04:14 +0200

    GET and POST requests

---
+++ app.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  //New id for the object
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+  //push the new tour to the tours array
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+const port = 3000;
+app.listen(port, function () {
+  console.log('listening on port 3000');
+});
